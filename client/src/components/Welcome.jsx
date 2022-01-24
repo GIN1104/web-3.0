f4a8080ba77d95c801ff5d34ff625f2e726995d1
Evgeny Starchenko <starchenkogin@gmail.com>
2022-01-24 13:43:37 +0200

    Welcome

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -22,10 +22,13 @@ const Welcome = () => {
     const connectWallet = () => {
 
     }
+     const handleSubmit =() => {
+
+     }
     return (
         <div className="flex w-full justify-center items-center ">
-        <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-            <div className="flex flex-1 justify-start flex-col md:mr-10">
+        <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+            <div className="flex flex-1 justify-start flex-col mf:mr-10">
             <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
                 Send Cripto <br/> across the world
             </h1>
@@ -61,7 +64,7 @@ const Welcome = () => {
                 </div>
             </div>
             </div>
-                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+                <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
                     <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism">
                      <div className="flex justify-between flex-col w-full h-full">
                         <div className="flex justify-between items-start">
@@ -90,14 +93,18 @@ const Welcome = () => {
                     <div className="h-[1px] w-full bg-gray-400 my-2"/>
 
                     {
-                        true ? (
+                        false ? (
                             <Loader />
                         ) 
                         : 
                         (
-                            <div>
-                              nothing
-                            </div>
+                            <button
+                                type="button"
+                                onClick={ handleSubmit }
+                                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                            >
+                              Send now
+                            </button>
                         )
                     }
                     </div>
