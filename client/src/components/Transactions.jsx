ca7260ab74e0ed7780728132506ac655450db169
Evgeny Starchenko <starchenkogin@gmail.com>
2022-02-01 18:58:47 +0200

    Working on Transactions component

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,8 +1,63 @@
+import React, { useContext } from 'react';
+
+import { TransactionContext } from '../context/TransactionContext';
+import dummyData from "../utils/dummyData";
+import { shortenAddress } from "../utils/shortenAddress";
+
+const TransactionCard =({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+    return  ( <div className="bg-[#181918] m-4 flex flex-1
+    2xl:min-w-[450px]
+    2xl:max-w-[450px]
+    sm:min-w-[270px]
+    sm:max-w-[300px]
+    flex-col p-3 rounded-t-md hover:shadow-2xl
+    ">
+    <div className="flex flex-col items-center w-full mt-3">
+    <div className="w-full mb-6 p-2">
+        <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noopener norefferrer">
+        <p className="text-white text-base ">From:  {shortenAddress(addressFrom)}</p>
+        </a>
+        <br/>
+        <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank"rel="noopener norefferrer">
+        <p className="text-white text-base ">To:  {shortenAddress(addressTo)}</p>
+        </a>
+        <p className="text-white text-base">Amount: {amount} ETH</p>
+        { message && (
+            <>
+             <br/>
+             <p className="text-white text-base"> Message: { message }</p>
+            </>
+        )}
+        <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+            <p className="text-[#37c7da] font-bold">{timestamp}</p>
+        </div>
+    </div>
+    </div>
+    </div>
+    )
+}
 
 const Transactions = () => {
+
+    const { currentAccount } = useContext( TransactionContext);
+ 
     return (
-        <div>
-           Transactions 
+        <div className="flex w-full justify-center itmes-center 2xl:px-20 gradient-bg-transactions">
+         <div className="flex flex-col md:p-12 py-12 px-4">
+            {currentAccount ? (
+                <h3 className="text-white text-3xl text-center my-2">Latest Transactions</h3>
+            ) : (
+                <h3 className="text-white text-3xl text-center my-2">Connect your account to see the Latest transactions</h3>
+            )
+            }
+            <div className="flex flex-wrap justify-center itmes-center mt-10 ">
+                {dummyData.reverse().map(( transaction, i) => (
+                    <TransactionCard key={transaction.id + i } {...transaction}/>
+                ))
+
+                }
+            </div>
+         </div>
         </div>
     )
 }
