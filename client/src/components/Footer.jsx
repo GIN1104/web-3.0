9dc91f6ab9dd5df57719cc587e6ac77cc5517bac
Evgeny Starchenko <starchenkogin@gmail.com>
2022-02-02 13:17:22 +0200

    Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,13 @@
+import logo from '../../images/logo.png';
 
 const Footer = () => {
     return (
-        <div>
-         Footer 
+        <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+        <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+        <div className="flex flex-[0.5] justify-center items-center">
+            <img src={logo} alt="logo" className="w-32"/>
+        </div>
+        </div>
         </div>
     )
 }
